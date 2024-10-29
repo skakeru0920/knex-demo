4ee92b21eb5ab1dcf72fbf98e445d947d758d69c
Kakeru Sano <skakeru0920@gmail.com>
2024-10-29 23:52:13 +0900

    initial

--- server.js
+++ server.js
@@ -1,22 +1,12 @@
-// expressのセットアップ
-const express = require('express');
+// express のセットアップ
+const express = require("express");
 const app = express();
 
-const config = require('./knexfile');
-const knex = require('knex')(config);
+// knex のセットアップ
+const config = require("./knexfile")["development"];
+const knex = require("knex")(config);
 
-app.get('/books', async (req, res) => {
-	// res.send("books");
-	const bookList = await knex('books').select();
-	// const bookList = await knex.select().from('books');
-	res.send(bookList);
-});
-
-app.post('/books', async (req, res) => {
-	const body = req.body;
-	await knex('books').insert(body);
-	res.status(201).end();
-});
+// sample データベースの books テーブルから全てのデータを取得して返すエンドポイントを作成する
 
 app.listen(3000, () => {
 	console.log(`サーバーが立ち上がりました: http://localhost:3000`);
